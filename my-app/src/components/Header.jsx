483930ee05e5ff8f090b8631d5e1ee80d3270eef
t-nakatani <tomoya.12@icloud.com>
2025-02-15 15:44:43 +0900

    feat: titleの強調

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -6,7 +6,7 @@ export const Header = ({ title }) => {
           <path d="M224,128a8,8,0,0,1-8,8H59.31l58.35,58.34a8,8,0,0,1-11.32,11.32l-72-72a8,8,0,0,1,0-11.32l72-72a8,8,0,0,1,11.32,11.32L59.31,120H216A8,8,0,0,1,224,128Z"></path>
         </svg>
       </div>
-      <h2 className="text-[#F8F9FB] text-lg font-bold leading-tight tracking-[-0.015em] flex-1 text-center pr-12">
+      <h2 className="text-[#F8F9FB] text-xl font-extrabold leading-tight tracking-[-0.015em] flex-1 text-center pr-12 drop-shadow-sm">
         {title}
       </h2>
     </div>

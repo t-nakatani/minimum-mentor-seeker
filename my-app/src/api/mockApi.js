242513efdb7d7f73b157a1f0da9ca6a1801af2a7
t-nakatani <tomoya.12@icloud.com>
2025-02-15 21:19:28 +0900

    feat: 特定の人に対してお願いするコンセプトに変更

--- my-app/src/api/mockApi.js
+++ my-app/src/api/mockApi.js
@@ -1,9 +1,9 @@
-const description = "Reactの基本的なコンポーネント作成と状態管理を勉強して理解したい"
+const description = "Reactの基本的なコンポーネント作成と状態管理について教えてほしい！"
 export const fetchPostData = async () => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve({
-        title: "Reactを理解したい",
+        title: "Reactの勉強手伝って",
         requester: {
           name: "Ludwig",
           avatar: "https://cdn.usegalileo.ai/sdxl10/913ab275-47b7-45f0-a7fd-bb7c50968339.png",
